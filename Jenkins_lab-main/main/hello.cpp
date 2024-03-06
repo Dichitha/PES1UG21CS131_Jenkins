8a6ecf7eec72f06293542572d89a00cc4046617d
Dichitha <92388117+Dichitha@users.noreply.github.com>
2024-03-07 01:21:45 +0530

    Update hello.cpp

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "This is modified\n";
+  cout << "webhook successfully enabled\n";
   return 0;
 }
